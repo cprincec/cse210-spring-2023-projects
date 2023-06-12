82d9f8271593a130f395d9248dc860668a117907
cprincec <chukwuprincec@gmail.com>
2023-06-12 21:54:09 +0100

    completed learning on polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle("Green", 5, 10);
+        Circle circle = new Circle("Yellow", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine(area);
+            Console.WriteLine(color);
+        }
     }
 }
